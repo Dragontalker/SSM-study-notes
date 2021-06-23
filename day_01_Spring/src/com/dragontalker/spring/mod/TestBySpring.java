9060ee603988851b7548ac732465d7bf274929ff
Richard <richard.yang.tong@gmail.com>
2021-06-23 15:31:40 -0400

    Added notes.

--- day_01_Spring/src/com/dragontalker/spring/mod/TestBySpring.java
+++ day_01_Spring/src/com/dragontalker/spring/mod/TestBySpring.java
@@ -9,8 +9,11 @@ public class TestBySpring {
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		Person person = context.getBean("person", Person.class);
-		System.out.println(person);
+		Person personOne = context.getBean("personOne", Person.class);
+		System.out.println(personOne);
+		
+		Person personTwo = context.getBean("personTwo", Person.class);
+		System.out.println(personTwo);
 		context.close();
 	}
 
