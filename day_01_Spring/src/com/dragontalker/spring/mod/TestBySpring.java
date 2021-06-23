6251701a009e3f24ab5fe775b317994b656dac20
Richard <richard.yang.tong@gmail.com>
2021-06-23 15:06:28 -0400

    Added notes.

--- day_01_Spring/src/com/dragontalker/spring/mod/TestBySpring.java
+++ day_01_Spring/src/com/dragontalker/spring/mod/TestBySpring.java
@@ -1,5 +1,13 @@
 package com.dragontalker.spring.mod;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestBySpring {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
 
 }
