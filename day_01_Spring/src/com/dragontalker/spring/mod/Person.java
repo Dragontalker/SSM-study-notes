aa2d9e4cc240d9dedd19f4d16b077afb40921170
Richard <richard.yang.tong@gmail.com>
2021-06-23 14:37:03 -0400

    Added notes.

--- day_01_Spring/src/com/dragontalker/spring/mod/Person.java
+++ day_01_Spring/src/com/dragontalker/spring/mod/Person.java
@@ -3,18 +3,27 @@ package com.dragontalker.spring.mod;
 public class Person {
 	
 	private Integer id;
+	
 	private String name;
 	
 	public Integer getId() {
 		return id;
 	}
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
 	}
 }
