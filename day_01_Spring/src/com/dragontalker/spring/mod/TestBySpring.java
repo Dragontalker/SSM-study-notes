823c243e1788b22175cdbe297b3f36ad3e929698
Richard <richard.yang.tong@gmail.com>
2021-06-23 15:08:51 -0400

    Added notes.

--- day_01_Spring/src/com/dragontalker/spring/mod/TestBySpring.java
+++ day_01_Spring/src/com/dragontalker/spring/mod/TestBySpring.java
@@ -8,6 +8,8 @@ public class TestBySpring {
 	public static void main(String[] args) {
 		
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Person p = ac.getBean("person", Person.class);
+		System.out.println(p);
 	}
 
 }
