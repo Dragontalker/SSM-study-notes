67da95221b0f19f2be31b2b11f900247232b13f9
Richard <richard.yang.tong@gmail.com>
2021-06-23 14:40:51 -0400

    Added notes.

--- day_01_Spring/src/com/dragontalker/spring/mod/Test.java
+++ day_01_Spring/src/com/dragontalker/spring/mod/Test.java
@@ -4,7 +4,10 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		
+		Person person = new Person();
+		person.setId(1001);
+		person.setName("ZhangSan");
+		System.out.println(person);
 	}
 
 }
