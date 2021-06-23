ea0ef0e3c4c9542f4eca96333db1858b6924fe05
Richard <richard.yang.tong@gmail.com>
2021-06-23 15:25:51 -0400

    Added notes.

--- day_01_Spring/src/com/dragontalker/spring/mod/TestBySpring.java
+++ day_01_Spring/src/com/dragontalker/spring/mod/TestBySpring.java
@@ -6,10 +6,9 @@ public class TestBySpring {
 	
 	public static void main(String[] args) {
 		
-		//初始化容器
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
-		//通过getBean()获取对象
+		
 		Person person = context.getBean("person", Person.class);
 		System.out.println(person);
 		context.close();
