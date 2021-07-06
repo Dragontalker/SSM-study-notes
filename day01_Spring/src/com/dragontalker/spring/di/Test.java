b62d7de1d5b7a5a6dc0751478d9179a82b1d9e13
Richard <richard.yang.tong@gmail.com>
2021-07-06 17:15:41 -0400

    Added notes.

--- day01_Spring/src/com/dragontalker/spring/di/Test.java
+++ day01_Spring/src/com/dragontalker/spring/di/Test.java
@@ -1,5 +1,17 @@
 package com.dragontalker.spring.di;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext ac = 
+				new ClassPathXmlApplicationContext("beans-di.xml");
+		
+		Student student = ac.getBean("s1", Student.class);
+		System.out.println(student);
+		
+		ac.close();
+	}
 }
