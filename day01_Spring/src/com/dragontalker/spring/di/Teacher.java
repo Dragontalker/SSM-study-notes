811624d32e349c44ac7f49ea772df0816d916313
Richard <richard.yang.tong@gmail.com>
2021-07-06 17:47:26 -0400

    Added notes.

--- day01_Spring/src/com/dragontalker/spring/di/Teacher.java
+++ day01_Spring/src/com/dragontalker/spring/di/Teacher.java
@@ -5,5 +5,21 @@ public class Teacher {
 	private Integer tId;
 	
 	private String name;
+
+	public Integer gettId() {
+		return tId;
+	}
+
+	public void settId(Integer tId) {
+		this.tId = tId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
