1610ed3a84ac40281e1df08ff6d614c1eed24d4a
Richard <richard.yang.tong@gmail.com>
2021-07-06 21:49:14 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/scope/Student.java
+++ day02_spring/src/com/dragontalker/ioc/scope/Student.java
@@ -13,4 +13,20 @@ public class Student {
 		this.name = name;
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
