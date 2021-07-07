9c19d299e1785c13aac0b2b59d7a20e23e485463
Richard <richard.yang.tong@gmail.com>
2021-07-07 10:03:12 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/userMod/dao/UserDaoImpl.java
+++ day02_spring/src/com/dragontalker/ioc/userMod/dao/UserDaoImpl.java
@@ -1,5 +1,11 @@
 package com.dragontalker.ioc.userMod.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserDaoImpl implements UserDao {
 
+	public UserDaoImpl() {
+		System.out.println(">> constructor UserDaoImp() called...");
+	}
 }
