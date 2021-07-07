c6685f6d145ad32eea85542ea56350315196c314
Richard <richard.yang.tong@gmail.com>
2021-07-07 07:25:44 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/Car.java
+++ day02_spring/src/com/dragontalker/ioc/Car.java
@@ -5,4 +5,26 @@ public class Car {
 	private Integer cid;
 	
 	private String cname;
+
+	public Integer getCid() {
+		return cid;
+	}
+
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [cid=" + cid + ", cname=" + cname + "]";
+	}
+	
 }
