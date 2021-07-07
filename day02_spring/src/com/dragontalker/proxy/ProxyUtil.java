96eed333778d5ec4805c40d27ec0ef7adeec5c5f
Richard <richard.yang.tong@gmail.com>
2021-07-07 17:43:04 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/proxy/ProxyUtil.java
+++ day02_spring/src/com/dragontalker/proxy/ProxyUtil.java
@@ -1,5 +1,25 @@
 package com.dragontalker.proxy;
 
-public class ProxyUtil {
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class ProxyUtil implements InvocationHandler{
+	
+	private Math mathImpl;
+	
+	public Object getProxy() {
+		
+		ClassLoader loader = this.getClass().getClassLoader();
+		Class[] interfaces = mathImpl.getClass().getInterfaces();
+		
+		return Proxy.newProxyInstance(loader, interfaces, this);
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
