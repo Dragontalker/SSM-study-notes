047ac2ed1ce4d682e891091823e8bc37d4473e25
Richard <richard.yang.tong@gmail.com>
2021-07-07 10:02:39 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/userMod/service/UserServiceImpl.java
+++ day02_spring/src/com/dragontalker/ioc/userMod/service/UserServiceImpl.java
@@ -1,5 +1,11 @@
 package com.dragontalker.ioc.userMod.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService {
 
+	public UserServiceImpl() {
+		System.out.println(">> consturctor UserServiceImpl() called...");
+	}
 }
