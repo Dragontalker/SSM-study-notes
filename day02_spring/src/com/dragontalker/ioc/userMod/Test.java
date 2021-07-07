d06dc5e9e859fd1b6c44d1d173f31f43eb776db1
Richard <richard.yang.tong@gmail.com>
2021-07-07 10:10:59 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/userMod/Test.java
+++ day02_spring/src/com/dragontalker/ioc/userMod/Test.java
@@ -1,8 +1,14 @@
 package com.dragontalker.ioc.userMod;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
 		
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("user.xml");
+		
+		context.close();
 	}
 }
