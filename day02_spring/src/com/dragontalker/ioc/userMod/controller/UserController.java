e4739c48eac17df7fab467d9fd98f35b881c153a
Richard <richard.yang.tong@gmail.com>
2021-07-07 15:50:14 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/userMod/controller/UserController.java
+++ day02_spring/src/com/dragontalker/ioc/userMod/controller/UserController.java
@@ -2,8 +2,16 @@ package com.dragontalker.ioc.userMod.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.dragontalker.ioc.userMod.service.UserService;
+
 @Controller
 public class UserController {
+	
+	private UserService userSerivce;
+	
+	public void addUser() {
+		
+	}
 
 	public UserController() {
 		System.out.println(">> constructor UserController() called....");
