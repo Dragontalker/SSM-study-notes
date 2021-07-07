096fe476ffa06edbb8f8354d602976b2360c153e
Richard <richard.yang.tong@gmail.com>
2021-07-07 18:14:01 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/proxy/MyLogger.java
+++ day02_spring/src/com/dragontalker/proxy/MyLogger.java
@@ -2,4 +2,11 @@ package com.dragontalker.proxy;
 
 public class MyLogger {
 
+	public static void before(String methodName, Object ... args) {
+		
+	}
+	
+	public static void afer(String methodName, String result) {
+		
+	}
 }
