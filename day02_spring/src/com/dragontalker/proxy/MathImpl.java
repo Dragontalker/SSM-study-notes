20514ca221972ea45d8dbf98cd8a72e8b0c140cd
Richard <richard.yang.tong@gmail.com>
2021-07-07 17:17:49 -0400

    Added notes.

---
+++ day02_spring/src/com/dragontalker/proxy/MathImpl.java
@@ -0,0 +1,29 @@
+package com.dragontalker.proxy;
+
+public class MathImpl implements MathInterface {
+
+	@Override
+	public int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int div(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
