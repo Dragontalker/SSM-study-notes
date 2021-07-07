ff18c1185fe60a4c82aa1de5879d271d9eefb14b
Richard <richard.yang.tong@gmail.com>
2021-07-07 17:17:25 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/proxy/MathInterface.java
+++ day02_spring/src/com/dragontalker/proxy/MathInterface.java
@@ -2,11 +2,11 @@ package com.dragontalker.proxy;
 
 public interface MathInterface {
 
-	int add(int i, int k);
+	int add(int i, int j);
 	
-	int sub(int i, int k);
+	int sub(int i, int j);
 	
-	int mul(int i, int k);
+	int mul(int i, int j);
 	
-	int div(int i, int k);
+	int div(int i, int j);
 }
