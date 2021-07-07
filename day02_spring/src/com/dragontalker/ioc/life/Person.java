ab74c3dc61bed8f088903edf152f5996ffdb6f9d
Richard <richard.yang.tong@gmail.com>
2021-07-06 22:10:42 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/life/Person.java
+++ day02_spring/src/com/dragontalker/ioc/life/Person.java
@@ -2,20 +2,23 @@ package com.dragontalker.ioc.life;
 
 public class Person {
 
-	private Integer age;
+	private Integer id;
 	
 	private String sex;
 	
 	private String name;
 	
-	public Person() {}
-
-	public Integer getAge() {
-		return age;
+	public Person() {
+		System.out.println("1. Constructor called...");
 	}
 
-	public void setAge(Integer age) {
-		this.age = age;
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		System.out.println("2. Dependency injected...");
+		this.id = id;
 	}
 
 	public String getSex() {
