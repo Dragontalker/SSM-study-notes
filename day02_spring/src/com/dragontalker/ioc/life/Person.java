be60e0828f32678c631a2498f1c79bbd335712b4
Richard <richard.yang.tong@gmail.com>
2021-07-06 22:09:10 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/life/Person.java
+++ day02_spring/src/com/dragontalker/ioc/life/Person.java
@@ -2,4 +2,36 @@ package com.dragontalker.ioc.life;
 
 public class Person {
 
+	private Integer age;
+	
+	private String sex;
+	
+	private String name;
+	
+	public Person() {}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
