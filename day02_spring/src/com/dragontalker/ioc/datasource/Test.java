d3cff284d8be8f7919f007ff9468e4edc09e1fda
Richard <richard.yang.tong@gmail.com>
2021-07-07 06:39:44 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/datasource/Test.java
+++ day02_spring/src/com/dragontalker/ioc/datasource/Test.java
@@ -1,19 +1,23 @@
 package com.dragontalker.ioc.datasource;
 
+import java.sql.SQLException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.alibaba.druid.pool.DruidDataSource;
 
 public class Test {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("datasource.xml");
 		
 		DruidDataSource bean = context.getBean("dataSource", DruidDataSource.class);
 		
-		System.out.println(bean);
+		System.out.println(bean.getConnection());
+		
+		context.close();
 		
 	}
 }
