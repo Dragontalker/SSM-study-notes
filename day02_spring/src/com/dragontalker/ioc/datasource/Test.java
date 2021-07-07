2593fca6e02153e38127f8436ba3c01298ce33c2
Richard <richard.yang.tong@gmail.com>
2021-07-07 06:36:57 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/datasource/Test.java
+++ day02_spring/src/com/dragontalker/ioc/datasource/Test.java
@@ -1,5 +1,19 @@
 package com.dragontalker.ioc.datasource;
 
-public class Test {
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.alibaba.druid.pool.DruidDataSource;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("datasource.xml");
+		
+		DruidDataSource bean = context.getBean("dataSource", DruidDataSource.class);
+		
+		System.out.println(bean);
+		
+	}
 }
