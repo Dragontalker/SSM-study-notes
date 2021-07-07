39f41b577f9b91f7d124395eecfa70372553996d
Richard <richard.yang.tong@gmail.com>
2021-07-07 07:26:53 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/Dept.java
+++ day02_spring/src/com/dragontalker/ioc/Dept.java
@@ -5,5 +5,21 @@ public class Dept {
 	private Integer did;
 	
 	private String dname;
+
+	public Integer getDid() {
+		return did;
+	}
+
+	public void setDid(Integer did) {
+		this.did = did;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
 	
 }
