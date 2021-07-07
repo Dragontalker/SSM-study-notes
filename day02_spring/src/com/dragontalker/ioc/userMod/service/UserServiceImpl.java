963db8d1f48c504be0834586f81d04b5f77a1c4a
Richard <richard.yang.tong@gmail.com>
2021-07-07 15:52:34 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/userMod/service/UserServiceImpl.java
+++ day02_spring/src/com/dragontalker/ioc/userMod/service/UserServiceImpl.java
@@ -11,7 +11,7 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public void addUser() {
-		// TODO Auto-generated method stub
+		System.out.println(">> addUser() @UserServiceImpl called...");
 		
 	}
 }
