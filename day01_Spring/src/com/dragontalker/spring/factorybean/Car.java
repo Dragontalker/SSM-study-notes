c9776d6038c7f5d422eb8f10f9b226c22c4e4fca
Richard <richard.yang.tong@gmail.com>
2021-07-06 21:19:42 -0400

    Added notes.

--- day01_Spring/src/com/dragontalker/spring/factorybean/Car.java
+++ day01_Spring/src/com/dragontalker/spring/factorybean/Car.java
@@ -2,4 +2,29 @@ package com.dragontalker.spring.factorybean;
 
 public class Car {
 
+	private String brand;
+	
+	private Double price;
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [brand=" + brand + ", price=" + price + "]";
+	}
+	
 }
