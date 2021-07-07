22035df8274fe7d8872a69ff42b22e943f7ae674
Richard <richard.yang.tong@gmail.com>
2021-07-06 20:57:40 -0400

    Added notes.

--- day01_Spring/src/com/dragontalker/spring/di/Test.java
+++ day01_Spring/src/com/dragontalker/spring/di/Test.java
@@ -26,10 +26,11 @@ public class Test {
 		
 		Teacher t1 = ac.getBean("t1", Teacher.class);
 		Teacher t2 = ac.getBean("t2", Teacher.class);
+		Teacher t3 = ac.getBean("t3", Teacher.class);
 		
 		System.out.println(t1);
 		System.out.println(t2);
-				
+		System.out.println(t3);
 		
 		ac.close();
 	}
