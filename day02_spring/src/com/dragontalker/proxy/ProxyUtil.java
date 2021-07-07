971219699a89a1356b6c06a03606f39702d9f32a
Richard <richard.yang.tong@gmail.com>
2021-07-07 18:19:52 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/proxy/ProxyUtil.java
+++ day02_spring/src/com/dragontalker/proxy/ProxyUtil.java
@@ -14,12 +14,14 @@ public class ProxyUtil {
 	}
 
 	public Object getProxy() {
-		
+		// 获取当前类的类加载器, 用来加载代理对象所属类
 		ClassLoader loader = this.getClass().getClassLoader();
+		// 获取目标对象实现的所有接口的Class, 代理类会和目标实现相同的接口, 最终通过代理对象实现功能
 		Class<?>[] interfaces = mathImpl.getClass().getInterfaces();
 		
 		return Proxy.newProxyInstance(loader, interfaces, new InvocationHandler() {
 			
+			// 代理对象实现功能的方式
 			@Override
 			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 				MyLogger.before(method.getName(), Arrays.toString(args));
