b759d8fb935492cdad888e9c92a08b879c5a7514
Richard <richard.yang.tong@gmail.com>
2021-07-06 21:49:02 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/scope/Student.java
+++ day02_spring/src/com/dragontalker/ioc/scope/Student.java
@@ -2,4 +2,15 @@ package com.dragontalker.ioc.scope;
 
 public class Student {
 
+	private Integer id;
+	
+	private String name;
+	
+	public Student() {}
+
+	public Student(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
 }
