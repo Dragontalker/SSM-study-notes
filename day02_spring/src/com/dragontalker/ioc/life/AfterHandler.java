7318303ff3dd9ae3945aad1c313552c1111baa42
Richard <richard.yang.tong@gmail.com>
2021-07-06 22:36:29 -0400

    Added notes.

--- day02_spring/src/com/dragontalker/ioc/life/AfterHandler.java
+++ day02_spring/src/com/dragontalker/ioc/life/AfterHandler.java
@@ -6,22 +6,20 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 public class AfterHandler implements BeanPostProcessor{
 
 	@Override
-	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 		Person person = (Person) bean;
-		
-		if (person.getSex().equals("男")) {
+		if(person.getSex().equals("男")) {
 			person.setName("张无忌");
-		} else {
+		}else {
 			person.setName("赵敏");
 		}
-		
 		return person;
 	}
 
 	@Override
-	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-
-		return null;
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		// TODO Auto-generated method stub
+		return bean;
 	}
 
 }
