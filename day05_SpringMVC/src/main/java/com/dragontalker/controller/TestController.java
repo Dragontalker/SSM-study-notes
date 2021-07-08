7792b538a14df9aa60ec6e50c3696e37b99140c0
Richard <richard.yang.tong@gmail.com>
2021-07-08 11:58:46 -0400

    Added notes.

--- day05_SpringMVC/src/main/java/com/dragontalker/controller/TestController.java
+++ day05_SpringMVC/src/main/java/com/dragontalker/controller/TestController.java
@@ -2,6 +2,7 @@ package com.dragontalker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
@@ -10,7 +11,7 @@ public class TestController {
 	 * @RequestMapping: 设置请求映射, 把请求和控制层中的方法设置映射关系
 	 * 当请求路径和@RequestMapping的value属性值一致时, 则该注解所标注的方法, 即为处理请求的方法
 	 */
-	@RequestMapping("/test")
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() {
 		
 		return "test";
