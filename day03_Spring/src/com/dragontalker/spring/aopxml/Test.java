16e0415e1d55ea48d5cf4ae4d71ee157a547a443
Richard <richard.yang.tong@gmail.com>
2021-07-08 09:47:17 -0400

    Added notes.

--- day03_Spring/src/com/dragontalker/spring/aopxml/Test.java
+++ day03_Spring/src/com/dragontalker/spring/aopxml/Test.java
@@ -1,8 +1,20 @@
 package com.dragontalker.spring.aopxml;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
 		
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("aop-xml.xml");
+		
+		MathImpl math = context.getBean("mathImpl", MathImpl.class);
+		
+		double add = math.add(12, 12);
+		
+		System.out.println(add);
+		
+		context.close();
 	}
 }
