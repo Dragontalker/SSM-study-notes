69c9d0ed1b8396efce00362854fb351ace622103
Richard <richard.yang.tong@gmail.com>
2021-07-08 11:48:10 -0400

    Added notes.

--- day05_SpringMVC/src/main/java/com/dragontalker/controller/TestController.java
+++ day05_SpringMVC/src/main/java/com/dragontalker/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.dragontalker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
-	
+	@RequestMapping("/test")
+	public String test() {
+		
+		return "test";
+	}
 }
