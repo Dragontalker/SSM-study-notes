b60fa196deb4c85614e74770813432365234eaed
Richard <richard.yang.tong@gmail.com>
2021-07-08 09:33:36 -0400

    Added notes.

---
+++ day03_Spring/src/com/dragontalker/spring/aopxml/Math.java
@@ -0,0 +1,12 @@
+package com.dragontalker.spring.aopxml;
+
+public interface Math {
+
+	double add(double i, double j);
+	
+	double sub(double i, double j);
+	
+	double mul(double i, double j);
+	
+	double div(double i, double j);
+}
---
+++ day03_Spring/src/com/dragontalker/spring/aopxml/MathImpl.java
@@ -0,0 +1,32 @@
+package com.dragontalker.spring.aopxml;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MathImpl implements Math {
+
+	@Override
+	public double add(double i, double j) {
+		double result = i + j;
+		return result;
+	}
+
+	@Override
+	public double sub(double i, double j) {
+		double result = i - j;
+		return result;
+	}
+
+	@Override
+	public double mul(double i, double j) {
+		double result = i * j;
+		return result;
+	}
+
+	@Override
+	public double div(double i, double j) {
+		double result = i / j;
+		return result;
+	}
+
+}
