11281631f412fc4ecc1a6eac86b741ffa06e63f5
Richard <richard.yang.tong@gmail.com>
2021-07-08 12:14:23 -0400

    Added notes.

--- day05_SpringMVC/src/main/java/com/dragontalker/controller/TestController.java
+++ day05_SpringMVC/src/main/java/com/dragontalker/controller/TestController.java
@@ -12,8 +12,9 @@ public class TestController {
 	 * 当请求路径和@RequestMapping的value属性值一致时, 则该注解所标注的方法, 即为处理请求的方法
 	 * method: 用来设置请求方式, 只有客户端发送请求方式的方式和method的值一致, 才能处理请求
 	 * 请求方式： GET(查询) POST(添加) PUT(修改) DELETE(删除)
+	 * params: 用来设置请求参数, 支持表达式
 	 */
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	@RequestMapping(value = "/test", method = RequestMethod.GET, params= { "username" })
 	public String testGET() {
 		
 		return "test";
