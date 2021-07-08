711adec50063f4c2f69121f8f0cb1cc68952b213
Richard <richard.yang.tong@gmail.com>
2021-07-08 06:19:48 -0400

    Added notes.

---
+++ day03_Spring/src/day03_Spring/spring/aop/MathImpl.java
@@ -0,0 +1,29 @@
+package day03_Spring.spring.aop;
+
+public class MathImpl implements Math {
+
+	@Override
+	public double add(double i, double j) {
+		double result = i + j;
+		return result;
+	}
+
+	@Override
+	public double sub(double i, double j) {
+		double result = i - j;
+		return result;
+	}
+
+	@Override
+	public double mul(double i, double j) {
+		double result = i * j;
+		return result;
+	}
+
+	@Override
+	public double div(double i, double j) {
+		double result = i / j;
+		return result;
+	}
+
+}
