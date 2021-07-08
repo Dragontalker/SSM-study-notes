dc5327e8d89716fede59fedfda03df8384a5368b
Richard <richard.yang.tong@gmail.com>
2021-07-08 06:30:15 -0400

    Added notes.

--- day03_Spring/src/day03_Spring/spring/aop/MyLoggerAspect.java
+++ day03_Spring/src/day03_Spring/spring/aop/MyLoggerAspect.java
@@ -1,8 +1,17 @@
 package day03_Spring.spring.aop;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
-@Aspect
+@Aspect // 标注当前类为切面
 public class MyLoggerAspect {
 
+	/**
+	 * @Before: 将方法指定为前置通知
+	 * 必须设置value, 其值为切入点表达式
+	 */
+	@Before(value = "")
+	public void beforeMethod() {
+		
+	}
 }
