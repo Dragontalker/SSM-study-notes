87cf5ca3565a652684d38edff0bea91061168a66
Richard <richard.yang.tong@gmail.com>
2021-07-09 06:30:00 -0400

    Added notes.

--- 06_SpringMVC/src/main/java/com/dragontalker/controller/TestController.java
+++ 06_SpringMVC/src/main/java/com/dragontalker/controller/TestController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
+	/**
+	 * View类型:
+	 * InternalResourceView： 转发视图
+	 * JstlView： 转发视图
+	 * RedirectView： 重定向视图
+	 */
 	@RequestMapping("/test")
 	public String test() {
 		
