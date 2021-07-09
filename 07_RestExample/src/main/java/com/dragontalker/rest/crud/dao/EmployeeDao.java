1ac5210b266740c36a33d2f594a7617c4ab77884
Richard <richard.yang.tong@gmail.com>
2021-07-09 07:39:58 -0400

    Added notes.

--- 07_RestExample/src/main/java/com/dragontalker/rest/crud/dao/EmployeeDao.java
+++ 07_RestExample/src/main/java/com/dragontalker/rest/crud/dao/EmployeeDao.java
@@ -1,10 +1,12 @@
 package com.dragontalker.rest.crud.dao;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.dragontalker.rest.crud.bean.Department;
 import com.dragontalker.rest.crud.bean.Employee;
 
 @Repository
@@ -14,5 +16,15 @@ public class EmployeeDao {
 	
 	@Autowired
 	private DepartmentDao departmentDao;
+	
+	static{
+		employees = new HashMap<Integer, Employee>();
+
+		employees.put(1001, new Employee(1001, "E-AA", "aa@163.com", 1, new Department(101, "D-AA")));
+		employees.put(1002, new Employee(1002, "E-BB", "bb@163.com", 1, new Department(102, "D-BB")));
+		employees.put(1003, new Employee(1003, "E-CC", "cc@163.com", 0, new Department(103, "D-CC")));
+		employees.put(1004, new Employee(1004, "E-DD", "dd@163.com", 0, new Department(104, "D-DD")));
+		employees.put(1005, new Employee(1005, "E-EE", "ee@163.com", 1, new Department(105, "D-EE")));
+	}
 
 }
