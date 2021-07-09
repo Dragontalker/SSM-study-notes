87fa1aedad3167ec60fe67025b0fed6441070d36
Richard <richard.yang.tong@gmail.com>
2021-07-09 19:50:41 -0400

    Added notes.

---
+++ Mybatis02/src/com/dragontalker/mapper/TestMybatis.java
@@ -0,0 +1,36 @@
+package com.dragontalker.mapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.dragontalker.bean.User;
+
+public class TestMybatis {
+
+	@Test
+	public void test() throws IOException {
+		
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		
+		SqlSessionFactory sqlSessionFactory = 
+				new SqlSessionFactoryBuilder().build(is);
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		// getMapper(): 会通过动态代理来动态生成UserMapper的代理实现类
+		UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+		
+		System.out.println(">> " + mapper.getClass().getName());
+		
+		User user = mapper.getUserByUid("1");
+		
+		System.out.println(">> " + user);
+	}
+
+}
