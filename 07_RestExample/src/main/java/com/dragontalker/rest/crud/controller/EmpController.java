3fd5ea14720f43228b9ad4064032131b2e369461
Richard <richard.yang.tong@gmail.com>
2021-07-09 08:36:59 -0400

    Added notes.

--- 07_RestExample/src/main/java/com/dragontalker/rest/crud/controller/EmpController.java
+++ 07_RestExample/src/main/java/com/dragontalker/rest/crud/controller/EmpController.java
@@ -1,8 +1,18 @@
 package com.dragontalker.rest.crud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.dragontalker.rest.crud.dao.DepartmentDao;
+import com.dragontalker.rest.crud.dao.EmployeeDao;
 
 @Controller
 public class EmpController {
+	
+	@Autowired
+	private EmployeeDao employeeDao;
+	
+	@Autowired
+	private DepartmentDao departmentDao;
 
 }
