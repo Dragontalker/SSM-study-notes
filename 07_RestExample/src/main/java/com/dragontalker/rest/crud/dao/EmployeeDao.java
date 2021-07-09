a4f47108633612b178b7adfd4a52fc6f02d0cf60
Richard <richard.yang.tong@gmail.com>
2021-07-09 07:39:28 -0400

    Added notes.

--- 07_RestExample/src/main/java/com/dragontalker/rest/crud/dao/EmployeeDao.java
+++ 07_RestExample/src/main/java/com/dragontalker/rest/crud/dao/EmployeeDao.java
@@ -1,8 +1,14 @@
 package com.dragontalker.rest.crud.dao;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
+
+import com.dragontalker.rest.crud.bean.Employee;
 
 @Repository
 public class EmployeeDao {
+	
+	private static Map<Integer, Employee> employees = null;
 
 }
