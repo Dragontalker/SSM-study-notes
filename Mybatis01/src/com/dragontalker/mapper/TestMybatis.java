054941fba018cadd15f39b5c3926d4d9b14138c0
Richard <richard.yang.tong@gmail.com>
2021-07-09 16:54:46 -0400

    Added notes.

---
+++ Mybatis01/src/com/dragontalker/mapper/TestMybatis.java
@@ -0,0 +1,14 @@
+package com.dragontalker.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMybatis {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
