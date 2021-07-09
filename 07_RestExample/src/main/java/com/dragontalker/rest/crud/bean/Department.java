1ec80a2032972dbc5bda4fdc30b3a54d85264919
Richard <richard.yang.tong@gmail.com>
2021-07-09 07:34:24 -0400

    Added notes.

--- 07_RestExample/src/main/java/com/dragontalker/rest/crud/bean/Department.java
+++ 07_RestExample/src/main/java/com/dragontalker/rest/crud/bean/Department.java
@@ -12,4 +12,20 @@ public class Department {
 		this.departmentName = departmentName;
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
 }
