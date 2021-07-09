d52acc807fa44fc66b9486ffe4b85add39cbdde6
Richard <richard.yang.tong@gmail.com>
2021-07-09 07:35:57 -0400

    Added notes.

--- 07_RestExample/src/main/java/com/dragontalker/rest/crud/bean/Employee.java
+++ 07_RestExample/src/main/java/com/dragontalker/rest/crud/bean/Employee.java
@@ -2,4 +2,13 @@ package com.dragontalker.rest.crud.bean;
 
 public class Employee {
 
+	private Integer id;
+	private String lastName;
+	private String email;
+	
+	// 1 male, 0 female
+	private Integer gender;
+	
+	private Department department;
+	
 }
