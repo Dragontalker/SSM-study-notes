013b37746d54a33b7e7c88c1be14733896069820
Richard <richard.yang.tong@gmail.com>
2021-07-09 16:25:55 -0400

    Added notes.

--- Mybatis01/src/com/dragontalker/bean/User.java
+++ Mybatis01/src/com/dragontalker/bean/User.java
@@ -2,4 +2,14 @@ package com.dragontalker.bean;
 
 public class User {
 
+	private Integer id;
+	
+	private String username;
+	
+	private String password;
+	
+	private Integer age;
+	
+	private String sex;
+	
 }
