6edf7c1a70019640c445bf67b0aaac1f4256297b
Richard <richard.yang.tong@gmail.com>
2021-07-09 17:04:41 -0400

    Added notes.

--- Mybatis01/src/com/dragontalker/mapper/TestMybatis.java
+++ Mybatis01/src/com/dragontalker/mapper/TestMybatis.java
@@ -6,9 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+
+import com.dragontalker.bean.User;
 
 public class TestMybatis {
 
@@ -16,8 +19,17 @@ public class TestMybatis {
 	public void test() throws IOException {
 		
 		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		
 		SqlSessionFactory sqlSessionFactory = 
 				new SqlSessionFactoryBuilder().build(is);
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+		
+		User user = mapper.getUserByUid("1");
+		
+		System.out.println(user);
 	}
 
 }
