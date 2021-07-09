c8446ff043a0b123775eb1724d72018fe725076c
Richard <richard.yang.tong@gmail.com>
2021-07-09 16:57:00 -0400

    Added notes.

--- Mybatis01/src/com/dragontalker/mapper/TestMybatis.java
+++ Mybatis01/src/com/dragontalker/mapper/TestMybatis.java
@@ -2,13 +2,18 @@ package com.dragontalker.mapper;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
 import org.junit.Test;
 
 public class TestMybatis {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws IOException {
+		
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
 	}
 
 }
