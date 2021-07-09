ac9e90982783ec1421a18724c6b05a853da6ce0d
Richard <richard.yang.tong@gmail.com>
2021-07-09 07:36:33 -0400

    Added notes.

--- 07_RestExample/src/main/java/com/dragontalker/rest/crud/bean/Employee.java
+++ 07_RestExample/src/main/java/com/dragontalker/rest/crud/bean/Employee.java
@@ -20,5 +20,45 @@ public class Employee {
 		this.gender = gender;
 		this.department = department;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 	
 }
