fe7ab9ef24ec8cb67514fca41a1a25e1af976535
Richard <richard.yang.tong@gmail.com>
2021-07-09 07:39:41 -0400

    Added notes.

--- 07_RestExample/src/main/java/com/dragontalker/rest/crud/dao/EmployeeDao.java
+++ 07_RestExample/src/main/java/com/dragontalker/rest/crud/dao/EmployeeDao.java
@@ -2,6 +2,7 @@ package com.dragontalker.rest.crud.dao;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.dragontalker.rest.crud.bean.Employee;
@@ -10,5 +11,8 @@ import com.dragontalker.rest.crud.bean.Employee;
 public class EmployeeDao {
 	
 	private static Map<Integer, Employee> employees = null;
+	
+	@Autowired
+	private DepartmentDao departmentDao;
 
 }
