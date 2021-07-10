fa5a6134853802785e9dc62078ac9f559e5bc96b
Richard <richard.yang.tong@gmail.com>
2021-07-09 20:00:49 -0400

    Added notes.

--- Mybatis01/user_table.sql
+++ Mybatis01/user_table.sql
@@ -17,4 +17,36 @@ CREATE TABLE t_user(
 INSERT t_user(`user_name`, `password`, `age`, `sex`) 
 VALUES("admin", "123456", 23, "male");
 
-SELECT * FROM t_user;
+DROP TABLE IF EXISTS emp;
+
+CREATE TABLE emp(
+	`eid` INT PRIMARY KEY AUTO_INCREMENT,
+    `ename` VARCHAR(255),
+    `age` INT,
+    `sex` VARCHAR(255)
+);
+
+INSERT INTO emp(`ename`, `age`, `sex`) 
+VALUES
+	("ZhangSan", 23, "male"),
+    ("LiSi", 12, "male"),
+    ("WangWu", 33, "male"),
+    ("ZhaoLiu", 22, "male"),
+    ("TianQi", 11, "male"),
+    ("CuiBa", 55, "male"),
+    ("XiaoMing", 44, "male"),
+    ("XiaoHong", 33, "male");
+
+DROP TABLE IF EXISTS dept;
+
+CREATE TABLE dept(
+	`did` INT PRIMARY KEY AUTO_INCREMENT,
+    `dname` VARCHAR(255)
+);
+
+INSERT INTO dept(`dname`)
+VALUES 
+	("Human Resource"),
+    ("Development"),
+    ("Research"),
+    ("Marketing");
