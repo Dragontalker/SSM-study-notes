8ce1b31b1907f2a664f440e07f8a02f5d16c9dd6
Richard <richard.yang.tong@gmail.com>
2021-07-10 07:51:17 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/test/TestParam.java
+++ Mybatis02/src/com/dragontalker/test/TestParam.java
@@ -1,5 +1,25 @@
 package com.dragontalker.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
 public class TestParam {
 
+	@Test
+	public void testUpdateEmp1() throws IOException {
+		
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		
+		
+	}
 }
