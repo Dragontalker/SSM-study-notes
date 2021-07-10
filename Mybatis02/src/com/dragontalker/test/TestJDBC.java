2b5b6cdfd0d952b317df1da2f8fe0d9a9214bf92
Richard <richard.yang.tong@gmail.com>
2021-07-10 08:15:35 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/test/TestJDBC.java
+++ Mybatis02/src/com/dragontalker/test/TestJDBC.java
@@ -3,6 +3,7 @@ package com.dragontalker.test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import com.dragontalker.bean.Emp;
@@ -13,11 +14,15 @@ public class TestJDBC {
 		
 		Class.forName("");
 		Connection conn = DriverManager.getConnection("", "", "");
-		PreparedStatement ps = conn.prepareStatement("insert into emp values(null, ?, ?, ?)");
+		PreparedStatement ps = 
+				conn.prepareStatement("insert into emp values(null, ?, ?, ?)", 1);
 		ps.setString(1,  "root");
 		ps.setString(2,  "23");
 		ps.setString(3,  "male");
 		ps.executeUpdate();
+		ResultSet rs = ps.getGeneratedKeys();
+		rs.next();
+		int id = rs.getInt(1);
 		
 		Statement statement = conn.createStatement();
 		Emp emp = new Emp(null, "a", 1, "b");
