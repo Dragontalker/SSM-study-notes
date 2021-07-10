4d061bcef6d36e9a0c745170868be120f30f188c
Richard <richard.yang.tong@gmail.com>
2021-07-09 20:02:00 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/bean/Emp.java
+++ Mybatis02/src/com/dragontalker/bean/Emp.java
@@ -51,4 +51,9 @@ public class Emp {
 		this.sex = sex;
 	}
 
+	@Override
+	public String toString() {
+		return "Emp [eid=" + eid + ", ename=" + ename + ", age=" + age + ", sex=" + sex + "]";
+	}
+
 }
