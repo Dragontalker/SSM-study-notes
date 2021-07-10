fb466e5592ea616f5e599705680cf7abe23da245
Richard <richard.yang.tong@gmail.com>
2021-07-09 20:41:16 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/test/TestCRUD.java
+++ Mybatis02/src/com/dragontalker/test/TestCRUD.java
@@ -2,6 +2,7 @@ package com.dragontalker.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -15,7 +16,7 @@ import com.dragontalker.mapper.EmpMapper;
 public class TestCRUD {
 
 	@Test
-	public void testCRUD() throws IOException {
+	public void testGetByEid() throws IOException {
 		
 		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
 		
@@ -28,5 +29,23 @@ public class TestCRUD {
 		Emp emp = empMapper.getEmpByEid("3");
 		
 		System.out.println(">> " + emp);
+	}
+	
+	@Test
+	public void testGetAll() throws IOException {
+		
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		EmpMapper empMapper = sqlSession.getMapper(EmpMapper.class);
+		
+		List<Emp> empList = empMapper.getAllEmp();
+		
+		for (Emp emp : empList) {
+			System.out.println(">> " + emp);
+		}
 	}
 }
