4bfc5e43935f5a31d6845b461318389eb037d1dc
Richard <richard.yang.tong@gmail.com>
2021-07-09 20:06:31 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/mapper/EmpMapper.java
+++ Mybatis02/src/com/dragontalker/mapper/EmpMapper.java
@@ -1,5 +1,7 @@
 package com.dragontalker.mapper;
 
+import java.util.List;
+
 import com.dragontalker.bean.Emp;
 
 public interface EmpMapper {
@@ -8,10 +10,14 @@ public interface EmpMapper {
 	Emp getEmpByEid(String eid);
 	
 	// 获取所有的员工信息
+	List<Emp> getAllEmp();
 	
 	// 添加员工信息
+	void addEmp(Emp emp);
 	
 	// 修改员工信息
+	void updateEmp(Emp emp);
 	
 	// 删除员工信息
+	void deleteEmp(String eid);
 }
