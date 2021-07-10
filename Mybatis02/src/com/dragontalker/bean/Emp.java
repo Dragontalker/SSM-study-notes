5d11bfb44e578bc6a833648317b6891f25dbf00f
Richard <richard.yang.tong@gmail.com>
2021-07-09 20:01:52 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/bean/Emp.java
+++ Mybatis02/src/com/dragontalker/bean/Emp.java
@@ -19,4 +19,36 @@ public class Emp {
 		this.sex = sex;
 	}
 
+	public Integer getEid() {
+		return eid;
+	}
+
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
 }
