58bb44341c544cb3388048a103c5fd50bf574fcf
Richard <richard.yang.tong@gmail.com>
2021-07-12 19:44:38 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/mapper/EmpMapper.java
+++ Mybatis03/src/com/dragontalker/mapper/EmpMapper.java
@@ -8,4 +8,7 @@ public interface EmpMapper {
 
 	// 根据eid, ename, age, sex多条件查询员工信息
 	List<Emp> getEmpListByMoreTJ(Emp emp);
+	
+	// 根据eid, ename, age, sex中的其中一个查询一个员工信息
+	List<Emp> getEmpListByChoose(Emp emp);
 }
