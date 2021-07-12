8db740d25e1655492e13713f0368da0c38a5a9af
Richard <richard.yang.tong@gmail.com>
2021-07-12 18:47:45 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/TestSQL.java
+++ Mybatis03/src/com/dragontalker/test/TestSQL.java
@@ -1,11 +1,23 @@
 package com.dragontalker.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 public class TestSQL {
 
 	@Test
-	public void test() {
+	public void testIf() {
 		
+	}
+	
+	public SqlSessionFactory getSqlSessionFactory() throws IOException {
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+		return sqlSessionFactory;
 	}
 }
