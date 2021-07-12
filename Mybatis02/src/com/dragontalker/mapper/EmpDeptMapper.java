244a1286c695e8d7bb48d4fd736f259b6d8b9a61
Richard <richard.yang.tong@gmail.com>
2021-07-12 16:19:34 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/mapper/EmpDeptMapper.java
+++ Mybatis02/src/com/dragontalker/mapper/EmpDeptMapper.java
@@ -2,6 +2,7 @@ package com.dragontalker.mapper;
 
 import java.util.List;
 
+import com.dragontalker.bean.Dept;
 import com.dragontalker.bean.Emp;
 
 public interface EmpDeptMapper {
@@ -9,4 +10,6 @@ public interface EmpDeptMapper {
 	List<Emp> getAllEmp();
 	
 	Emp getEmpStep(String eid);
+	
+	Dept getDeptByDid(String did);
 }
