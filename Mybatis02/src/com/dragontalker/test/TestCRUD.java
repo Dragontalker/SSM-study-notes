cb11ce345d9cf51eaa2219b1ba6aeaed60485f4a
Richard <richard.yang.tong@gmail.com>
2021-07-12 10:34:10 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/test/TestCRUD.java
+++ Mybatis02/src/com/dragontalker/test/TestCRUD.java
@@ -4,12 +4,23 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+
+import com.dragontalker.mapper.EmpMapper;
 
 public class TestCRUD {
 
 	@Test
 	public void testCRUD() throws IOException {
 		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		EmpMapper empMapper = sqlSession.getMapper(EmpMapper.class);
+		
 	}
 }
