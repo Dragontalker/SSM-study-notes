37938a1e5cd4b3acbb4b98e96ee1d8514ce2463e
Richard <richard.yang.tong@gmail.com>
2021-07-12 11:14:01 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/test/TestCRUD.java
+++ Mybatis02/src/com/dragontalker/test/TestCRUD.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import com.dragontalker.bean.Emp;
 import com.dragontalker.mapper.EmpMapper;
 
 public class TestCRUD {
@@ -37,7 +36,10 @@ public class TestCRUD {
 		sqlSession.commit(); // 提交事务*/
 		
 		// 测试: 修改员工信息
-		empMapper.updateEmp(new Emp(2, "ZhangSanSan", 33, "female"));
+		//empMapper.updateEmp(new Emp(2, "ZhangSanSan", 33, "female"));
+		
+		// 测试: 删除员工信息
+		empMapper.deleteEmp("5");
 	}
 	
 }
