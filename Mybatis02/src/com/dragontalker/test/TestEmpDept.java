7f6646e89618ef43c90c04273208054e4fdecc88
Richard <richard.yang.tong@gmail.com>
2021-07-12 15:25:01 -0400

    Added notes.

---
+++ Mybatis02/src/com/dragontalker/test/TestEmpDept.java
@@ -0,0 +1,27 @@
+package com.dragontalker.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.dragontalker.bean.Emp;
+import com.dragontalker.mapper.ParamMapper;
+
+public class TestEmpDept {
+
+	@Test
+	public void testCRUD() throws IOException {
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+		SqlSession sqlSession = sqlSessionFactory.openSession(true); 
+		ParamMapper mapper = sqlSession.getMapper(ParamMapper.class);
+		
+		
+	}
+	
+}
