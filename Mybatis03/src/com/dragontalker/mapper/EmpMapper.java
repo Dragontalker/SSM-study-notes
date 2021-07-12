c006c36f9032948c8b4c4e14679fffd865acc8eb
Richard <richard.yang.tong@gmail.com>
2021-07-12 18:49:49 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/mapper/EmpMapper.java
+++ Mybatis03/src/com/dragontalker/mapper/EmpMapper.java
@@ -1,6 +1,11 @@
 package com.dragontalker.mapper;
 
+import java.util.List;
+
+import com.dragontalker.bean.Emp;
+
 public interface EmpMapper {
 
-
+	// 根据eid, ename, age, sex多条件查询员工信息
+	List<Emp> getEmpListByMoreTJ();
 }
