fd9d297a5ceebf280762e0cd02c6c1f57274639e
Richard <richard.yang.tong@gmail.com>
2021-07-12 10:32:47 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/test/TestCRUD.java
+++ Mybatis02/src/com/dragontalker/test/TestCRUD.java
@@ -1,11 +1,15 @@
 package com.dragontalker.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
 import org.junit.Test;
 
 public class TestCRUD {
 
 	@Test
-	public void testCRUD() {
-		
+	public void testCRUD() throws IOException {
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
 	}
 }
