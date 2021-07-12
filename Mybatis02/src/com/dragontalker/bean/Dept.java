25e45dd85a5fa75e44a84ff750b7dc2740fd2804
Richard <richard.yang.tong@gmail.com>
2021-07-12 10:16:45 -0400

    Added notes.

--- Mybatis02/src/com/dragontalker/bean/Dept.java
+++ Mybatis02/src/com/dragontalker/bean/Dept.java
@@ -6,4 +6,20 @@ public class Dept {
 	
 	private String dname;
 
+	public Integer getDid() {
+		return did;
+	}
+
+	public void setDid(Integer did) {
+		this.did = did;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
 }
