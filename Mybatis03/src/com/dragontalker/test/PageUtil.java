72f867e03472f8a1027e0cb27244bcea06b3302c
Richard <richard.yang.tong@gmail.com>
2021-07-13 15:36:02 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/PageUtil.java
+++ Mybatis03/src/com/dragontalker/test/PageUtil.java
@@ -11,5 +11,8 @@ public class PageUtil {
 	public void getPageInfo(PageInfo<Emp> pageInfo) {
 		
 		StringBuilder builder = new StringBuilder();
+		
+		// 拼接首页
+		builder.append("<a href='emps>首页</a>");
 	}
 }
