fcf852a21cee4320e0841f7692a5217d85d667e2
Richard <richard.yang.tong@gmail.com>
2021-07-13 06:05:01 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/TestCache.java
+++ Mybatis03/src/com/dragontalker/test/TestCache.java
@@ -9,16 +9,28 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import com.dragontalker.bean.Emp;
+import com.dragontalker.mapper.EmpMapper;
+
 public class TestCache {
 	
 	/**
-	 * mybatis中的一级缓存默认开启, 是SqlSession级别的
+	 * MyBatis中的一级缓存默认开启, 是SqlSession级别的
+	 * 即同一个SqlSession对于一个SQL语句, 执行之后就会存储在缓存中, 
+	 * 下次执行相同的SQL, 直接从缓存中取 
 	 */
 	@Test
 	public void testCache() throws IOException {
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		
+		EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+		
+		Emp emp1 = mapper.getEmpByEid("13");
+		System.out.println(emp1);
+		
+		Emp emp2 = mapper.getEmpByEid("13");
+		System.out.println(emp2);
 	}
 
 	
