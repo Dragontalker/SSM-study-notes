5b3e5b452e9ab7381854bc30a4c0dfca88f268b2
Richard <richard.yang.tong@gmail.com>
2021-07-13 09:32:25 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/TestSQL.java
+++ Mybatis03/src/com/dragontalker/test/TestSQL.java
@@ -1,14 +1,9 @@
 package com.dragontalker.test;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 public class TestSQL {
