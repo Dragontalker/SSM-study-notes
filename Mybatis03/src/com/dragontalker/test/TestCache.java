f69b6b5bcc2b01ef7006f38dcc88f874843f2e01
Richard <richard.yang.tong@gmail.com>
2021-07-13 05:57:21 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/TestCache.java
+++ Mybatis03/src/com/dragontalker/test/TestCache.java
@@ -1,5 +1,18 @@
 package com.dragontalker.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class TestCache {
 
+	
+	public SqlSessionFactory getSqlSessionFactory() throws IOException {
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+		return sqlSessionFactory;
+	}
 }
