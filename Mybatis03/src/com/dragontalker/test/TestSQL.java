e3ad07bda3e7f2e8920c4d6f6ed4e3bf2924d7bd
Richard <richard.yang.tong@gmail.com>
2021-07-13 09:46:38 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/TestSQL.java
+++ Mybatis03/src/com/dragontalker/test/TestSQL.java
@@ -21,30 +21,21 @@ public class TestSQL {
 	@Test
 	public void testCRUD() throws Exception{
 		
-		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		//SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		//SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		
 	}
 	
 	@Test
 	public void testMBG() throws Exception {
-		
 		List<String> warnings = new ArrayList<String>();
-		
 		boolean overwrite = true;
-		
 		File configFile = new File("mbg.xml");
-		
 		ConfigurationParser cp = new ConfigurationParser(warnings);
-		
 		Configuration config = cp.parseConfiguration(configFile);
-		
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-		
 		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-		
 		myBatisGenerator.generate(null);
-		
 	}
 	
 	public SqlSessionFactory getSqlSessionFactory() throws Exception {
