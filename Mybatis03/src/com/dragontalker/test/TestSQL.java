308d59dd6cdf16fe33e3b6910d2970b3370c967c
Richard <richard.yang.tong@gmail.com>
2021-07-13 09:33:54 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/TestSQL.java
+++ Mybatis03/src/com/dragontalker/test/TestSQL.java
@@ -6,17 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
 
 public class TestSQL {
 	
 	@Test
-	public void testMBG() throws IOException {
+	public void testMBG() throws Exception {
 		
 		List<String> warnings = new ArrayList<String>();
 		
 		boolean overwrite = true;
 		
 		File configFile = new File("mbg.xml");
+		
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		
+		Configuration config = cp.parseConfiguration(configFile);
 		
 		
 		
