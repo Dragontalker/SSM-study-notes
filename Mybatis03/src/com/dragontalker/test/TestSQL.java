f2217e57ae9f7268a084b1d7281b7bea3a55faca
Richard <richard.yang.tong@gmail.com>
2021-07-13 07:29:49 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/TestSQL.java
+++ Mybatis03/src/com/dragontalker/test/TestSQL.java
@@ -10,74 +10,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import com.dragontalker.bean.Emp;
-import com.dragontalker.mapper.EmpMapper;
-
 public class TestSQL {
 	
 	@Test
 	public void testMore() throws IOException {
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
-		EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
 		
-		//List<Integer> eids = new ArrayList<>();
-		//eids.add(7);
-		//eids.add(8);
-		//eids.add(10);
-		
-		//Emp[] emps = new Emp[];
-		//Emp[] emps = new Emp[] {};
-		
-		Emp emp1 = new Emp(17, "a1", 233, "female");
-		Emp emp2 = new Emp(18, "aa1", 233, "female");
-		Emp emp3 = new Emp(19, "aaa1", 233, "female");
-		Emp[] emps = {emp1, emp2, emp3};
-		
-		
-		mapper.updateMoreByArray(emps);
-	}
-	
-	@Test
-	public void testChoose() throws IOException {
-		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
-		EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
-		
-		Emp emp = new Emp();
-		
-		//emp.setEid(1);
-		emp.setEname("ZhangSan");
-		emp.setAge(23);
-		emp.setSex("123");
-		
-		mapper.insertEmp(emp);
-		
-		//List<Emp> list = mapper.getEmpListByChoose(emp);
-		
-		//for (Emp e :list) {
-			//System.out.println(e);
-		//}
-	}
 
-	@Test
-	public void testIf() throws IOException {
-		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
-		EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
-		
-		Emp emp = new Emp();
-		
-		//emp.setEid(1);
-		emp.setEname("ZhangSan");
-		emp.setAge(23);
-		//emp.setSex("1");
-		
-		List<Emp> list = mapper.getEmpListByMoreTJ(emp);
-		
-		for (Emp e :list) {
-			System.out.println(e);
-		}
 	}
 	
 	public SqlSessionFactory getSqlSessionFactory() throws IOException {
