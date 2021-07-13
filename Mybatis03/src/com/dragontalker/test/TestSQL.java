dbb930f18edb5afa966a59932fd718493bcaed07
Richard <richard.yang.tong@gmail.com>
2021-07-13 09:34:57 -0400

    Added notes.

--- Mybatis03/src/com/dragontalker/test/TestSQL.java
+++ Mybatis03/src/com/dragontalker/test/TestSQL.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class TestSQL {
 	
@@ -24,7 +26,11 @@ public class TestSQL {
 		
 		Configuration config = cp.parseConfiguration(configFile);
 		
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
 		
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		
+		myBatisGenerator.generate(null);
 		
 	}
 	
